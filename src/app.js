2fdf42e40432c4bd4c1019b535d485e29a9c1468
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-02-14 19:17:21 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// listado de opciones de excusas //
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateExcuse(who, action, what, when) {
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+  console.log("Text");
+
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
+const randomExcuse = generateExcuse(who, action, what, when);
+const selectorhtml = document.getElementById("excuse");
+selectorhtml.textContent = randomExcuse;
+
+document.getElementById("generateExcuseBtn").addEventListener("click", () => {
+  location.reload();
+});
